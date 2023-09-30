bc5ef67423383318a58717684e9f2807cbeeb559
Mike Kistler <mikekistler@microsoft.com>
2023-09-30 10:19:18 -0700

    Add Contoso Fiber project

--- typespec-library/src/lib.ts
+++ typespec-library/src/lib.ts
@@ -2,13 +2,13 @@ import { createTypeSpecLibrary } from "@typespec/compiler";
 import { versionPolicyRule } from "./rules/versionPolicy.rule.js";
 
 export const myLibrary = createTypeSpecLibrary({
-  name: "ApiLibrary",
+  name: "@contoso/typespec-library",
   diagnostics: {},
   linter: {
     rules: [versionPolicyRule],
     ruleSets: {
       recommended: {
-        enable: { [`${versionPolicyRule.name}`]: true },
+        enable: { [`@contoso/typespec-library:${versionPolicyRule.name}`]: true },
       },
     },
   },
